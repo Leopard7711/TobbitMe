31fdf9ce2c8312d4db96184513285ae43278e238
Leopard7711 <leopard7711@naver.com>
2024-02-18 15:51:46 +0900

    02-18-2

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app =express();
+const path = require('path');
+
+let staticPath = path.join(__dirname, 'css');
+console.log(`Serving static files from: ${staticPath}`);
+app.use(express.static(staticPath));
+
+staticPath = path.join(__dirname, 'src');
+console.log(`Serving static files from: ${staticPath}`);
+app.use(express.static(staticPath));
+
 app.listen(80,function(){
     console.log('listening on 80')
     
